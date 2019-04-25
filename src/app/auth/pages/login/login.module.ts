528cef340f9a0267e0397c0817c806913291a80d
Weldys Santos <weldyss@gmail.com>
2019-04-24 22:50:57 -0300

    first page with login

---
+++ src/app/auth/pages/login/login.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { SharedModule } from 'src/app/shared/shared.module';
+
+import { LoginPage } from './login.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginPage
+  }
+];
+
+@NgModule({
+  imports: [
+    SharedModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [LoginPage]
+})
+export class LoginPageModule {}
