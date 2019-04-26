80d3d3639db23924c5313246b40028f6529079c4
Weldys Santos <weldyss@gmail.com>
2019-04-26 20:11:03 -0300

    adding auth and overlay service

--- src/app/auth/pages/login/login.page.ts
+++ src/app/auth/pages/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { AuthProvider } from 'src/app/core/services/auth.types';
 
 @Component({
   selector: 'app-login',
@@ -9,6 +11,7 @@ import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms'
 export class LoginPage implements OnInit {
 
   authForm: FormGroup;
+  authProviders = AuthProvider;
   configs = {
     isSignIn: true,
     action: 'login',
@@ -17,7 +20,7 @@ export class LoginPage implements OnInit {
 
   private nameControl = new FormControl('', [Validators.required, Validators.minLength(3)]);
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private authService: AuthService, private fb: FormBuilder) { }
 
   ngOnInit() {
     this.createForm();
@@ -47,13 +50,23 @@ export class LoginPage implements OnInit {
     const {isSignIn} = this.configs;
     this.configs.action = isSignIn ? 'Login' : 'Sign up';
     this.configs.actionChange = isSignIn ? 'Create account' : 'I already have an account';
-    !isSignIn 
-      ? this.authForm.addControl('name', this.nameControl) 
-      : this.authForm.removeControl('name')
+    !isSignIn
+      ? this.authForm.addControl('name', this.nameControl)
+      : this.authForm.removeControl('name');
   }
 
-  onSubmit(): void {
-    console.log('Authform', this.authForm.value);
+  async onSubmit(provider: AuthProvider): Promise<void> {
+    try {
+      const credentials = await this.authService.authenticate({
+        isSignIn: this.configs.isSignIn,
+        user: this.authForm.value,
+        provider
+      });
+      console.log('Authenticated', credentials);
+      console.log('Redirecting...');
+    } catch (e) {
+      console.log('Auth Error', e);
+    }
   }
 
 }
--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,27 +1,54 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { auth, User } from 'firebase/app';
+import { auth } from 'firebase/app';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { User, AuthProvider, AuthOptions } from './auth.types';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  constructor(private afAuth: AngularFireAuth) {}
 
-  private signInWithEmailAndPassword({ email, password }): Promise<auth.UserCredential> {
+  authState$: Observable<firebase.User>;
+
+  constructor(private afAuth: AngularFireAuth) {
+    this.authState$ = this.afAuth.authState;
+  }
+
+  get isAuthenticated(): Observable<boolean> {
+    return this.authState$.pipe(map(user => user !== null));
+  }
+
+  authenticate({isSignIn, provider, user}: AuthOptions): Promise<void | auth.UserCredential> {
+    let operation: Promise<void | auth.UserCredential>;
+    if (provider !== AuthProvider.Email) {
+      operation = this.signInWithPopup(provider);
+    } else {
+      operation = isSignIn ? this.signInWithEmailAndPassword(user) : this.signUpWithEmail(user);
+    }
+    return operation;
+  }
+
+  logout(): Promise<void> {
+    return this.afAuth.auth.signOut();
+  }
+
+  private signInWithEmailAndPassword({ email, password }: User): Promise<auth.UserCredential> {
     return this.afAuth.auth.signInWithEmailAndPassword(email, password);
   }
 
-  private signUpWithEmail({ name, email, password }): Promise<void | auth.UserCredential> {
+  private signUpWithEmail({ name, email, password }: User): Promise<void | auth.UserCredential> {
     return this.afAuth.auth.createUserWithEmailAndPassword(email, password).then(credentials => {
       credentials.user.updateProfile({ displayName: name, photoURL: null }).then(() => credentials);
     });
   }
 
-  private signInWithPopup(provider: string): Promise<auth.UserCredential> {
+  private signInWithPopup(provider: AuthProvider): Promise<auth.UserCredential> {
     let signInProvider = null;
     switch (provider) {
-      case 'facebook':
+      case AuthProvider.Facebook:
         signInProvider = new auth.FacebookAuthProvider();
         break;
     }
