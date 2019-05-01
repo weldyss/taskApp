1aa3a856aaa515f448e22f40aef7b2c7a2e91822
Weldys Santos <weldyss@gmail.com>
2019-05-01 09:07:50 -0300

    crud of tasks and user associating

--- src/app/pages/tasks-list/tasks-list.page.ts
+++ src/app/pages/tasks-list/tasks-list.page.ts
@@ -1,23 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Task } from 'src/app/tasks/models/task.model';
+import { TasksService } from 'src/app/tasks/services/tasks.service';
 
 @Component({
   selector: 'app-tasks-list',
   templateUrl: './tasks-list.page.html',
-  styleUrls: ['./tasks-list.page.scss'],
+  styleUrls: ['./tasks-list.page.scss']
 })
 export class TasksListPage implements OnInit {
-
   tasks$: Observable<Task[]>;
 
-  constructor() { }
+  constructor(private tasksService: TasksService) {}
 
   ngOnInit() {
-    this.tasks$ = of([
-      {id: 'dadasdaslkkhohkdfd', title: 'Aprender Ionic', done: false},
-      {id: 'dsa334gfddasdasdad', title: 'Aprender Angular 4', done: true}
-    ])
+    this.tasks$ = this.tasksService.getAll();
   }
-
 }
---
+++ src/app/tasks/services/tasks.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+
+import { AuthService } from 'src/app/core/services/auth.service';
+import { Firestore } from 'src/app/core/classes/firestore.class';
+import { Task } from '../models/task.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TasksService extends Firestore<Task> {
+
+  constructor(private authService: AuthService, db: AngularFirestore) {
+    super(db);
+    this.init();
+  }
+
+  private init(): void {
+    this.authService.authState$.subscribe(user => {
+      if (user) {
+        this.setCollection(`/users/${user.uid}/tasks`);
+        return;
+      }
+      this.setCollection(null);
+    })
+  }
+}
