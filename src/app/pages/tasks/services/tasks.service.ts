3776d692ead1fdb0546508005436892b0403cc12
Weldys Santos <weldyss@gmail.com>
2019-05-06 22:23:37 -0300

    adding menu button

--- src/app/pages/tasks/services/tasks.service.ts
+++ src/app/pages/tasks/services/tasks.service.ts
@@ -4,12 +4,12 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import { AuthService } from 'src/app/core/services/auth.service';
 import { Firestore } from 'src/app/core/classes/firestore.class';
 import { Task } from '../models/task.model';
+import { firestore } from 'firebase';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TasksService extends Firestore<Task> {
-
   constructor(private authService: AuthService, db: AngularFirestore) {
     super(db);
     this.init();
@@ -18,10 +18,12 @@ export class TasksService extends Firestore<Task> {
   private init(): void {
     this.authService.authState$.subscribe(user => {
       if (user) {
-        this.setCollection(`/users/${user.uid}/tasks`);
+        this.setCollection(`/users/${user.uid}/tasks`, ref =>
+          ref.orderBy('done', 'asc').orderBy('title', 'asc')
+        );
         return;
       }
       this.setCollection(null);
-    })
+    });
   }
 }
