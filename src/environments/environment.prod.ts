63e79d334bb22ae8fcfa37b78b3659e402d43649
Weldys Santos <weldyss@gmail.com>
2019-05-07 23:57:46 -0300

    adding fireabse hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC4AlCXZ60A5KrA4ggbwmYWnRE7wZJqGF4',
+    authDomain: 'ionicfiretasks.firebaseapp.com',
+    databaseURL: 'https://ionicfiretasks.firebaseio.com',
+    projectId: 'ionicfiretasks',
+    storageBucket: 'ionicfiretasks.appspot.com',
+    messagingSenderId: '695832744871'
+  }
 };
